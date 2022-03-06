9cb70ff50a510f9d32f7d9285d1881109fd027fc
n643064 <n6430@tutanota.com>
2022-03-06 01:58:12 +0100

    add tf2 hud colorscheme and update move script

--- move.sh
+++ move.sh
@@ -2,3 +2,4 @@
 
 cp .zshrc $HOME/.zshrc
 cp .config/terminator/config $HOME/.config/terminator/config
+cp clientscheme_colors.res $HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf/custom/rayshud/resource/scheme/
