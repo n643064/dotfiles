e2ae145916d7ee87e247d3731550ce574364cb20
n643064 <n6430@tutanota.com>
2022-04-13 20:47:46 +0200

    Change terminal config

--- move.sh
+++ move.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cp .zshrc $HOME/.zshrc
-cp .config/terminator/config $HOME/.config/terminator/config
-cp clientscheme_colors.res $HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf/custom/rayshud/resource/scheme/
-cp clientscheme_borders.res $HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf/custom/rayshud/resource/scheme/
+cp .zshrc $HOME/
+cp kitty.conf $HOME/.config/kitty/
+cp colorscheme.conf $HOME/.config/kitty/
