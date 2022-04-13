518704d4e92ed02f6f307076cf744c878eae2b4c
n643064 <n6430@tutanota.com>
2022-04-13 20:43:35 +0200

    Switch from youtube-dl to yt-dlp

--- move.sh
+++ move.sh
@@ -3,3 +3,4 @@
 cp .zshrc $HOME/.zshrc
 cp .config/terminator/config $HOME/.config/terminator/config
 cp clientscheme_colors.res $HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf/custom/rayshud/resource/scheme/
+cp clientscheme_borders.res $HOME/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf/custom/rayshud/resource/scheme/
