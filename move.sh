98095bc0eaa70c504cfbacd8642db606f29a74bf
n643064 <n643064@tutanota.com>
2023-04-30 00:41:08 +0200

    update move.sh

--- move.sh
+++ move.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-cp .zshrc $HOME/
-cp .zshrc-ydl $HOME/
+#cp .zshrc $HOME/
+#cp .zshrc-ydl $HOME/
+cp .bashrc $HOME/
+cp .inputrc $HOME/
+
 cp kitty.conf $HOME/.config/kitty/
 cp colorscheme.conf $HOME/.config/kitty/
 cp neofetch.conf $HOME/.config/neofetch/config.conf
