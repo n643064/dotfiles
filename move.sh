ab43feb5c21309f59c89f105b91717f394cf542e
n643064 <n643064@tutanota.com>
2024-11-13 13:45:03 +0100

    I forgor :skull:

--- move.sh
+++ move.sh
@@ -2,12 +2,12 @@
 
 #cp .zshrc $HOME/
 #cp .zshrc-ydl $HOME/
-cp .bashrc $HOME/
-cp .inputrc $HOME/
+cp .bashrc "$HOME/"
+cp .inputrc "$HOME/"
 
-mkdir -pv .config/kitty
-mkdir -pv .config/neofetch
+mkdir -pv "$HOME/.config/kitty"
+mkdir -pv "$HOME/.config/neofetch"
 
-cp kitty.conf $HOME/.config/kitty/
-cp colorscheme.conf $HOME/.config/kitty/
-cp neofetch.conf $HOME/.config/neofetch/config.conf
+cp kitty.conf "$HOME/.config/kitty/"
+cp colorscheme.conf "$HOME/.config/kitty/"
+cp neofetch.conf "$HOME/.config/neofetch/config.conf"
